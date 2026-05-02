c73f6dabe48cab14ba01c80421712bde93fb9674
zylen <276438148+zzylenn@users.noreply.github.com>
2026-05-02 01:05:17 +0100

    Add files via upload

---
+++ me/index.js
@@ -0,0 +1,54 @@
+const enter = document.getElementById("enter")
+const video=document.getElementById("video")
+const audio=document.getElementById("audio")
+const cursor=document.querySelector(".cursor")
+
+let mouseX=0
+let mouseY=0
+let currentX=0
+let currentY=0
+
+document.addEventListener("mousemove",e=>{
+  mouseX=e.clientX
+  mouseY=e.clientY
+})
+
+function animate(){
+  currentX+=(mouseX-currentX)*0.12
+  currentY+=(mouseY-currentY)*0.12
+  cursor.style.left=currentX+"px"
+  cursor.style.top=currentY+"px"
+  requestAnimationFrame(animate)
+}
+
+animate()
+
+document.querySelectorAll(".icon").forEach(el=>{
+  el.addEventListener("mouseenter",()=>{
+    cursor.style.transform="translate(-50%,-50%) scale(1.4)"
+  })
+  el.addEventListener("mouseleave",()=>{
+    cursor.style.transform="translate(-50%,-50%) scale(1)"
+  })
+})
+
+enter.addEventListener("click",()=>{
+  enter.classList.add("hidden")
+  video.classList.add("show")
+  video.play()
+  audio.volume=0.4
+  audio.play()
+})
+
+document.getElementById("dc").onclick=()=>window.open("<https://discord.com/users/1443089229679951973>","_blank")
+document.getElementById("gh").onclick=()=>window.open("https://github.com/zzylenn","_blank")
+document.getElementById("rb").onclick=()=>window.open("https://roblox.com/users/3116923731/","_blank")
+document.getElementById("st").onclick=()=>window.open("https://steamcommunity.com/id/zzylenn/","_blank")
+
+document.querySelectorAll(".icon").forEach(btn=>{
+  btn.addEventListener("mousemove",e=>{
+    const r=btn.getBoundingClientRect()
+    btn.style.setProperty("--x",`${e.clientX-r.left}px`)
+    btn.style.setProperty("--y",`${e.clientY-r.top}px`)
+  })
+})
